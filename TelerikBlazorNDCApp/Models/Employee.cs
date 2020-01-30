5899fd7c42af15c02cfe3075b6dd24c17666f4e2
Joana <Joana.Ivanova@telerik.com>
2020-01-30 10:08:31 +0200

    feat(app): implement skeleton

---
+++ TelerikBlazorNDCApp/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TelerikBlazorNDCApp.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+        }
+
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string FirstName { get; set; }
+
+        [StringLength(30)]
+        public string Title { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+
+        public DateTime? HireDate { get; set; }
+    }
+}
---
+++ TelerikBlazorNDCApp/Services/GridDataService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TelerikBlazorNDCApp.Models;
+
+namespace TelerikBlazorNDCApp.Services
+{
+    public class GridDataService
+    {
+        public async Task<List<Employee>> GetEmployeesAsync()
+        {
+            return await Task.FromResult(GetEmployees());
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return new List<Employee>()
+            {
+                new Employee()
+                {
+                    EmployeeId = 1,
+                    FirstName = "Yoana",
+                    LastName = "Ivanova",
+                    Title = "Software Engineer",
+                    BirthDate = new DateTime(1992, 12, 10),
+                    HireDate = new DateTime(2012, 11, 19)
+                }
+            };
+        }
+    }
+}
